60bb5fd5a64e7dd092493f97073837c419fb0778
hyunsongyeon <55427981+hyunsongyeon@users.noreply.github.com>
2019-11-26 15:34:04 +0900

    01

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+struct student{
+		int ID;
+		char name[10];
+		double grade;
+	};
+void main(int argc, char *argv[]) {
+	
+	struct student s1 = {24, "각자이름", 4.3};
+	s1.ID = 1712941;
+	strcpy(s1.name, "Songyeon");
+	s1.grade = 4.0;
+	
+	printf("ID : %d\n", s1.ID);
+	printf("name : %s\n", s1.name);
+	printf("grade : %f\n", s1.grade);
+	
+	
+}
